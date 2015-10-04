bea30cd602cce91c2c6895a6fbfcdf7fc7c0cf60
John Carty <john.carty@gmail.com>
2015-10-04 15:02:52 -0400

    Initial Commit

---
+++ Week7-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Week7-2
+//
+//  Created by John Carty on 10/4/15.
+//  Copyright © 2015 John Carty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
